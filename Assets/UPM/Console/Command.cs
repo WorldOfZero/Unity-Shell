ba5a1e73619061dc2f7546e5e29ada7bb01e782b
TurretBox <godsofgamin@gmail.com>
2017-06-29 21:17:11 +0200

    Command:   - added Custom Command Name CommandDiscovery:   - added Function to get All Assemblies in the Unity Project, and added that in the Build()   - Tried to add a Execute function for Non-Static methodes, but we have to get the Type, so we can make an Activator/Instance of that class with the Methode ConsoleWindow:   - added Mini ToolBar   - added Copy/Paste   - Fixed so the GUISkin("named Layout") is being globaly used (but this makes the scrollBars black, because the GUISkin has that as the image) SEE LINE: 137   - added Preferences for the ConsoleWindow, this only contains a toggle for AutoScroll   - added Auto Scroll to the bottom when you are typing/ when you are executing a new line   - added Log from default Unity Console

--- Assets/UPM/Console/Command.cs
+++ Assets/UPM/Console/Command.cs
@@ -4,9 +4,13 @@ using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 
-
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = false)]
 public class CommandAttribute : System.Attribute
 {
+    public string CustomName = ""; // maybe we wanna make a command named something that already exists, we can just add a custom name and use that instead :)
+                              // like this [Command(CustomName = "NameMe")]
+
+    public bool OnlyIngame = false; // maybe we make it so commands marked with this can only be run while the game is playing
 }
 
 public class Command
--- Assets/UPM/Editor/Console/ConsoleWindow.cs
+++ Assets/UPM/Editor/Console/ConsoleWindow.cs
@@ -8,66 +8,224 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class ConsoleWindow : EditorWindow
 {
-    private StringBuilder currentLine = new StringBuilder();
-    private List<string> previousCommands = new List<string>();
-    private Vector2 scrollPosition = Vector2.zero;
+    /// <summary>
+    /// Clipboard: makes it so we can Copy and Paste stuff :)
+    /// </summary>
+    public static string clipboard
+    {
+        get { return GUIUtility.systemCopyBuffer; }
+        set { GUIUtility.systemCopyBuffer = value; }
+    }
 
+
+    private StringBuilder currentLine = new StringBuilder(); // current command line
+    private static List<string> previousCommands = new List<string>(); // console contents / previousCommands
+
+    //UI variables
+    private bool ConsoleLogEnabled = true; // should this console display stuff from the default unity console =?
+    private static bool ScrollToBottomOnNewCommand = true; // should the scrollview scroll to the bottom when entering a new command ?
+    private Vector2 scrollPosition = Vector2.zero; // current ScrollPosition
     public GUISkin layout;
 
     [MenuItem("Tools/Unity Shell %`")]
     static void InitializeWindow()
     {
         var window = GetWindow<ConsoleWindow>("Unity Shell", true);
+        window.titleContent.tooltip = "Unity Shell"; // fix me
         window.autoRepaintOnSceneChange = true;
         window.Show();
         window.layout = Resources.Load<GUISkin>("Skins/UnityShellDefault");
     }
 
+    #region Preferences
+    private static bool PreferencesLoaded = false;
+
+    [PreferenceItem("Unity Shell")]
+    public static void PreferencesGUI()
+    {
+        //get values    (happens on start up)
+        if (!PreferencesLoaded)
+        {
+            //editorprefs something
+            EditorPrefs.GetBool("ScrollToBottmWhenNewCommand", true);
+            PreferencesLoaded = true;
+        }
+
+        //GUI that is showing   (editorGuiLayout stuff)
+        EditorGUILayout.HelpBox("Hello, I'm testing this PreferencesGUI(), thingy \nand its working okey :)", MessageType.Info);
+        EditorGUILayout.BeginHorizontal(GUILayout.ExpandWidth(true));
+
+        bool NewValue1 = EditorPrefs.GetBool("ScrollToBottmWhenNewCommand", true);
+        EditorGUILayout.LabelField("Auto Scroll down when Typing");
+        NewValue1 = EditorGUILayout.Toggle(NewValue1);
+
+        EditorGUILayout.EndHorizontal();
+
+        //set values    (happens when you change the values in prefrences)
+        if (GUI.changed)
+        {
+            EditorPrefs.SetBool("ScrollToBottmWhenNewCommand", NewValue1);
+            PrefsUpdate();
+            //editorprefs something
+        }
+    }
+
+    public static void PrefsUpdate()
+    {
+        ScrollToBottomOnNewCommand = EditorPrefs.GetBool("ScrollToBottmWhenNewCommand", true);
+    }
+    #endregion
+
+    #region OnEnable and OnDisable/OnDestroy
+    void OnDestroy()
+    {
+        Application.logMessageReceived -= HandleConsole;
+    }
+    void OnDisable()
+    {
+        Application.logMessageReceived -= HandleConsole;
+    }
+    void OnEnable()
+    {
+        Application.logMessageReceived -= HandleConsole; // reset before, setting it again.. dont know why i cant use !=
+        Application.logMessageReceived += HandleConsole;
+    }
+    #endregion
+
+    void HandleConsole(string logmessage, string trace, LogType type)
+    {
+        if (ConsoleLogEnabled)
+        {
+            Log("<color=grey>"+logmessage+"</color>"); // just adding a custom color, so its easier to see :)
+            Repaint();
+        }
+    }
+
     public void OnGUI()
     {
+        #region Events
         if (EditorWindow.focusedWindow == this)
         {
-            if (Event.current != null)
+            Event e = Event.current; // the current event
+
+            switch (e.type)
             {
-                if (Event.current.type == EventType.KeyDown)
-                {
-                    switch (Event.current.keyCode)
+                //this Event makes it posible todo Copy and Paste
+                case EventType.ValidateCommand:
+
+                    //Debug.Log("CommandName: " + e.commandName);
+                    if (e.commandName == "Paste")
+                    {
+                        currentLine.Append(clipboard); // paste from clipboard
+                    }
+                    else if (e.commandName == "Copy")
+                    {
+                        clipboard = currentLine.ToString(); // set new clipboard
+                    }
+
+                    e.Use(); // use the event 
+                    Repaint();
+                    break;
+
+                case EventType.KeyDown:
+                    switch (e.keyCode)
                     {
                         case KeyCode.Backspace:
-                            currentLine = currentLine.Remove(currentLine.Length - 1, 1);
+                            if(currentLine.Length > 0) // fixes so you cant remove nothing
+                                currentLine = currentLine.Remove(currentLine.Length - 1, 1);
                             break;
 
                         case KeyCode.Return:
-                            CommandDiscovery.Build(this.GetType().Assembly, typeof(CommandDiscovery).Assembly).Invoke(currentLine.ToString());
-                            previousCommands.Add(currentLine.ToString());
+
+                            if (currentLine.Length == 0) // dont execute nothing, and/or make a new empty line
+                                return;
+
+                            previousCommands.Add("> " + currentLine.ToString());
+                            CommandDiscovery.Build().Invoke(currentLine.ToString()); // sorry for changing, but i added a function that gets All the Project Assemblies inside this unity project, but if we cant get an Assembly we can manualy add it
                             currentLine = currentLine.Remove(0, currentLine.Length);
+
+                            if (ScrollToBottomOnNewCommand) // scrolls the scrollView to the bottom so we can see what we executed :)
+                                scrollPosition = new Vector2(scrollPosition.x, float.MaxValue); // <- this actualy works :D
                             break;
 
                         default:
-                            currentLine = currentLine.Append(Event.current.character);
+                            currentLine = currentLine.Append(e.character);
+
+                            if (ScrollToBottomOnNewCommand) // scrolls the scrollView to the bottom so we can see what we typed :)
+                                scrollPosition = new Vector2(scrollPosition.x, float.MaxValue); // <- this actualy works :D
                             break;
                     }
+
                     //Debug.Log("Current Keycode: " + Event.current.keyCode.ToString() + " Character: " +
                     //          Event.current.character);
                     currentLine.Replace("\n", "");
                     currentLine.Replace("\0", "");
-                    Event.current.Use();
+
+                    e.Use();
                     this.Repaint();
-                } else if (Event.current.type == EventType.KeyUp)
-                {
-                    Event.current.Use(); // Use the current event on Key Up so other elements don't mess with it.
-                }
+                    break;
+
+                case EventType.KeyUp:
+                    e.Use();
+                    break;
             }
         }
+        #endregion
 
-        scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, false, true, this.layout.horizontalScrollbar, this.layout.verticalScrollbar, this.layout.scrollView);
+        #region GUILayout
+        GUI.skin = layout; //globaly sets the GUISkin 
+
+        #region Mini ToolBar
+        //TODO: make the toolbar using the custom GUISkin, instead of default unity
+        EditorGUILayout.BeginHorizontal(EditorStyles.toolbar, GUILayout.ExpandWidth(true));
+        if (GUILayout.Button("Clear", EditorStyles.toolbarButton, GUILayout.Width(45f)))
+        {
+            ClearLog();
+        }
+
+        if (GUILayout.Button("Save", EditorStyles.toolbarButton, GUILayout.Width(45f)))
+        {
+            //save not added atm
+        }
+
+        ConsoleLogEnabled = GUILayout.Toggle(ConsoleLogEnabled, "Console Log", EditorStyles.toolbarButton, GUILayout.Width(100f));
+
+        GUILayout.FlexibleSpace();
+
+        EditorGUILayout.EndHorizontal();
+        #endregion
+
+        #region ScrollArea
+        Rect ContentRect = new Rect(0, 16, position.width, position.height-16); // 16 + 16 = 32   and 3 from the left.
+        GUILayout.BeginArea(ContentRect);
+
+        scrollPosition = GUILayout.BeginScrollView(scrollPosition, false, true, this.layout.horizontalScrollbar, this.layout.verticalScrollbar, this.layout.scrollView);
+        GUILayout.BeginVertical();
+
         foreach (var previous in previousCommands)
         {
-            EditorGUILayout.LabelField("> " + previous, this.layout.label);
+            GUILayout.Label(previous, this.layout.label);
         }
-        EditorGUILayout.LabelField("> " + currentLine.ToString(), this.layout.label);
-        
-        EditorGUILayout.EndScrollView();
+
+        GUILayout.Label("> " + currentLine.ToString(), this.layout.label);
+
+        GUILayout.EndVertical();
+        GUILayout.EndScrollView();
+        GUILayout.EndArea();
+
+        #endregion
+
+        #endregion
+    }
+    [Command(CustomName = "Clear")] // added custom names, so we can have names that are simpler than the Methode name
+    public static void ClearLog()
+    {
+        previousCommands = new List<string>();
+    }
+
+    public void Log(string message)
+    {
+        previousCommands.Add(message);
     }
 
     [Command]
